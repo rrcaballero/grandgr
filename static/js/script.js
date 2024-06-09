929fae450827aeb74e7fcc998255ad42d4de45ab
rrcaballero <rrcaballero88@gmail.com>
2024-06-09 20:04:04 +0300

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -62,27 +62,6 @@ anchors.forEach(anchor => {
 
 
 
-$('body').append('<div class="upbtn"></div>');
-$(window).scroll(function() {
-	if ($(this).scrollTop() > 100) {
-		$('.upbtn').css({
-			transform: 'scale(1)'
-		});
-        } else {
-		$('.upbtn').css({
-			transform: 'scale(0)'
-		});
-	}
-});
-$('.upbtn').on('click',function() {
-	$('html, body').animate({
-		scrollTop: 0
-	}, 500);
-	return false;
-});
-
-
-
 
 
 
@@ -152,3 +131,103 @@ for (let a = 0; a < animHeading.length; a++){
 	}
 }
 new WOW().init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function() {
+	$('.forma').each(function() {	
+		var $frm = $(this);
+		var input = $(this).find('.validate-input-at .input-at');
+		var butsend = $(this).find('.form-at-btn');
+		butsend.on('click',function(){
+			var check = true;
+			for(var i=0; i<input.length; i++) {
+				if(validate(input[i]) == false){
+					showValidate(input[i]);
+					check=false;
+				}
+			}
+			// Отправка формы		
+			if (check == true) {
+				$.post("static/php/send.php", $frm.find(".form-at select, .form-at input, .form-at textarea").serialize(),
+					function(data){
+						if(data.frm_check == 'error'){ 
+							$frm.find(".result-at").html("<div class='error-at'>Ошибка: " + data.msg + "</div>");					
+							} else {
+							$frm.find(".result-at").html("<div class='success-at'>Ваше сообщение отправлено!</div>"); 
+							$frm.find(".form-at").fadeOut(500);
+							$frm.find(".input-at").val("");			
+						}
+					}, "json");
+					return false;
+			}
+		});
+		$('.form-at .input-at').each(function(){
+			$(this).focus(function(){
+				hideValidate(this);
+			});
+		});
+		
+	});	
+	function validate(input) {
+		
+			if($(input).attr('type') == 'email' || $(input).attr('name') == 'email-at') {
+			if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+			return false;
+			}
+			}
+		
+		if($(input).val().trim() == ''){
+			return false;
+		}
+	}
+	function showValidate(input) {
+		var thisAlert = $(input).parent();
+		$(thisAlert).addClass('alert-validate');
+	}
+	function hideValidate(input) {
+		var thisAlert = $(input).parent();
+		$(thisAlert).removeClass('alert-validate');
+	}
+});
